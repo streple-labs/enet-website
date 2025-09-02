4f846ee290c2a8d5021398df175a54bba2e78de1
dLuxKid <adetunjimarvellous09@gmail.com>
2025-09-02 20:29:17 +0100

    done mobile nav

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { IoCloseOutline } from "react-icons/io5";
 
 const navItems = [
   { href: "/", label: "Home" },
@@ -16,9 +19,15 @@ const navItems = [
 export default function Navbar() {
   const pathname = usePathname();
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen((prev) => !prev);
+  }
+
   return (
-    <header className="flex justify-center relative">
-      <nav className="flex mw items-center bg-[#0000001A] backdrop-blur-[50px] py-3 md:py-5 px-[30px] lg:px-20 w-full justify-between">
+    <header className="flex flex-col justify-center relative">
+      <nav className="hidden lg:flex mw items-center bg-[#0000001A] backdrop-blur-[50px] py-3 md:py-5 px-[30px] lg:px-20 w-full justify-between">
         <Link href={"/"}>
           <Image
             loading="lazy"
@@ -26,11 +35,49 @@ export default function Navbar() {
             alt="navbar logo"
             width={160}
             height={51}
-            className="w-[75px] h-6 lg:w-[160px] lg:h-[51px] flex-shrink-0"
+            className="flex-shrink-0"
           />
         </Link>
 
-        <div className="hidden lg:flex flex-row items-center gap-12 font-normal text-[18px] capitalize">
+        <div className="flex items-center gap-12 font-normal text-[18px] capitalize">
+          {navItems.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className={`${
+                pathname === item.href
+                  ? "font-bold uppercase"
+                  : "font-normal capitalize"
+              }`}
+            >
+              {item.label}
+            </Link>
+          ))}
+        </div>
+
+        <Link href={"#"}>
+          <button
+            className="py-[12px] px-[26px] border border-white rounded-[8px] font-bold text-[18px]"
+            title="book a call"
+            aria-label="book a call"
+          >
+            Book a Call
+          </button>
+        </Link>
+      </nav>
+      <nav className="flex lg:hidden mw items-center bg-[#0000001A] backdrop-blur-[50px] py-3 md:py-5 px-[30px] lg:px-20 w-full justify-between">
+        <Link href={"/"}>
+          <Image
+            loading="lazy"
+            src="/enet-logo-nav.png"
+            alt="navbar logo"
+            width={75}
+            height={24}
+            className="flex-shrink-0"
+          />
+        </Link>
+
+        <div className="hidden lg:flex items-center gap-12 font-normal text-[18px] capitalize">
           {navItems.map((item) => (
             <Link
               key={item.href}
@@ -53,7 +100,7 @@ export default function Navbar() {
           Book a Call
         </Link>
 
-        <div className="lg:hidden">
+        <button onClick={toggleMenu}>
           <svg
             width="18"
             height="11"
@@ -80,8 +127,60 @@ export default function Navbar() {
               strokeLinecap="round"
             />
           </svg>
-        </div>
+        </button>
       </nav>
+
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            initial={{ x: "-100%", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: "-100%", opacity: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="fixed inset-0 z-50 flex flex-col bg-[#030e14] px-4 pt-6"
+          >
+            <div className="flex justify-end">
+              <button
+                onClick={toggleMenu}
+                aria-label="Close menu"
+                className="text-[#FFFFFFCC] cursor-pointer"
+                type="button"
+              >
+                <IoCloseOutline size={28} />
+              </button>
+            </div>
+
+            {/* mobile menu items */}
+            <div className="my-auto">
+              <div className="flex flex-col text-center gap-6 font-normal text-base">
+                {navItems.map((item) => (
+                  <Link
+                    key={Math.random()}
+                    href={item.href}
+                    onClick={toggleMenu}
+                    className={`${
+                      pathname === item.href
+                        ? "font-bold uppercase"
+                        : "font-normal capitalize"
+                    }`}
+                  >
+                    {item.label}
+                  </Link>
+                ))}
+              </div>
+
+              <div className="flex flex-col gap-3 mt-12 text-center">
+                <Link
+                  href={"#"}
+                  className="hidden lg:block py-[12px] px-[26px] border border-white rounded-[8px] font-bold text-[18px]"
+                >
+                  Book a Call
+                </Link>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </header>
   );
 }
